78c4ddcda9a401e949d235e76a1df2868b46d7b5
theludicnebula <theludicnebula@gmail.com>
2014-04-23 16:36:02 -0500

    adding files

---
+++ run_analysis.R
@@ -0,0 +1,49 @@
+#x_test <- read.table("UCI HAR Dataset\\test\\X_test.txt")
+#y_test <- read.table("UCI HAR Dataset\\test\\Y_test.txt")
+#subject_test <- read.table("UCI HAR Dataset\\test\\subject_test.txt")
+#head(y_test)
+library("plyr")
+testfiles <- list.files("UCI HAR Dataset/test/", pattern=("txt$"), recursive=TRUE)
+testfiles <- testfiles[!is.na(testfiles)]
+trainfiles <-list.files("UCI HAR Dataset/train/", pattern=("txt$"), recursive=TRUE)
+testfiles <- trainfiles[!is.na(trainfiles)]
+
+#Global
+activity_labels <- read.table("UCI HAR Dataset//activity_labels.txt")
+features <- read.table("UCI HAR Dataset//features.txt")
+
+#Test
+subject_test<- read.table("UCI HAR Dataset//test//subject_test.txt")
+x_test<- read.table("UCI HAR Dataset//test//X_test.txt")
+y_test<- read.table("UCI HAR Dataset//test//Y_test.txt")
+body_acc_x_test <- read.table("UCI HAR Dataset//test//Inertial Signals//body_acc_x_test.txt")
+
+#build test 
+names(x_test) <- features[,2]
+test_set <- subject_test
+names(test_set) <- c("Subject")
+test_set <- cbind(test_set, x_test)
+
+test_activities <- join(y_test, activity_labels)
+names(test_activities) <- c("Activity_Code", "Activity_Name")
+test_set <- cbind(test_set, test_activities)
+
+#Train
+subject_train<- read.table("UCI HAR Dataset//train//subject_train.txt")
+x_train<- read.table("UCI HAR Dataset//train//X_train.txt")
+y_train<- read.table("UCI HAR Dataset//train//Y_train.txt")
+#build Train
+
+names(x_train) <- features[,2]
+train_set <- subject_train
+names(train_set) <- c("Subject")
+train_set <- cbind(train_set, x_train)
+
+train_activities <- join(y_train, activity_labels)
+names(train_activities) <- c("Activity_Code", "Activity_Name")
+train_set <- cbind(train_set, train_activities)
+
+
+#Merge by subject
+#merge(test_set, train_set, by="Subject")
+combined_data <- rbind(test_set, train_set)
