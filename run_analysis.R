1d5495e1b89d4525d3eee3d0d4bcf8a06b5ddb75
theludicnebula <theludicnebula@gmail.com>
2014-04-24 14:31:30 -0500

    scripts finished

--- run_analysis.R
+++ run_analysis.R
@@ -3,6 +3,9 @@
 #subject_test <- read.table("UCI HAR Dataset\\test\\subject_test.txt")
 #head(y_test)
 library("plyr")
+
+#download file
+
 testfiles <- list.files("UCI HAR Dataset/test/", pattern=("txt$"), recursive=TRUE)
 testfiles <- testfiles[!is.na(testfiles)]
 trainfiles <-list.files("UCI HAR Dataset/train/", pattern=("txt$"), recursive=TRUE)
@@ -47,6 +50,22 @@ train_set <- cbind(train_set, train_activities)
 #Merge by subject
 #merge(test_set, train_set, by="Subject")
 combined_data <- rbind(test_set, train_set)
+
+#get the Mean and Standard Deviation columns
 means <- combined_data[,grepl("[Mm]ean", names(combined_data))]
 stds <- combined_data[,grepl("[Ss]td", names(combined_data))]
-cleaned_data<-cbind(combined_data$Subject, combined_data$Activity_Name, means, stds)
+subjects <- data.frame(combined_data$Subject)
+activities <- data.frame(combined_data$Activity_Name)
+names(subjects) <- c("Subject")
+names(activities) <- c("Activity_Name")
+
+
+#merge into one data frame
+cleaned_data<-cbind(subjects, activities, means, stds)
+
+#Subject into Activites and Subjects and then take the mean of columns
+Activity_Subject_Averages <- ddply(cleaned_data, .(Activity_Name, Subject), numcolwise(mean))
+#remove Subject
+
+
+#get averages
